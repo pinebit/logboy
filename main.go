f73786aabd4542cbd96ce1a25d1f09aa91c82948
Andrei Smirnov <andrei.smirnov@smartcontract.com>
2023-02-25 11:19:03 +0300

    Better config

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/pinebit/lognite/app"
 )
 
 const defaultConfigPath = "config/config.yaml"
 
 func main() {
-	app := app.NewApp(defaultConfigPath)
+	flag.Parse()
+	if flag.NArg() > 1 {
+		fmt.Println("Usage: lognite [path-to-config-yaml]")
+		os.Exit(1)
+	}
+
+	configPath := defaultConfigPath
+	if flag.NArg() == 1 {
+		configPath = flag.Arg(0)
+	}
+
+	app := app.NewApp(configPath)
 	defer app.Close()
 
 	app.Run()
